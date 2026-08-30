92d721d1414d081df1773001f1beb073e59586fd
zcm <lsj88lyr@gmail.com>
2026-08-30 14:56:12 +0800

    fix(manager): missing ArrayList import in LogsFragment

--- app/src/main/java/org/lsposed/manager/ui/fragment/LogsFragment.java
+++ app/src/main/java/org/lsposed/manager/ui/fragment/LogsFragment.java
@@ -65,6 +65,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
